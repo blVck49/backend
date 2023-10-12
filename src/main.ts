636683a1decc307fe58e2052455f22951f17808e
blVck49 <adebiyitope49@gmail.com>
2023-10-12 20:01:09 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cors from "cors";
+
+
+// var whitelist = ['http://localhost',]
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
   await app.listen(3000);
+
+
 }
 bootstrap();
